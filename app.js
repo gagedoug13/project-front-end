35c615a0240a4778fcccdb1d6a65d6d2140d4507
Gage Thornberry <gagedoug13@gmail.com>
2019-10-02 16:33:55 -0600

    styled the home page and sighting page

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const body = document.body
 
 function createCards(users){
     let div = document.createElement("div")
-    div.classname = "cards"
+    div.className = "cards"
 
     users.forEach(user => {
-        let p = document.createElement("p")
         let a = document.createElement("a")
-
+        
         a.href = `users.html?id=${user.id}`
-        a.innerText = `${user.name}`
-
+        a.innerText = `${user.name}\n`
 
         div.append(a)
     })
